bf9b2faa32791967bbbcdc2a8d6a231d7e439b27
Aaron <aaronlal61@gmail.com>
2023-12-29 08:16:21 +1100

    Initial Commit

---
+++ CS50Seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CS50Seminar
+//
+//  Created by Aaron on 29/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
