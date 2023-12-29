2f37044d513b2e12f89d33ad995db270ad3ee765
Aaron <aaronlal61@gmail.com>
2023-12-29 11:39:02 +1100

    cs50 2022 - seminar - iOS development

--- CS50Seminar/ViewController.swift
+++ CS50Seminar/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var emojiLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 
-
+    @IBAction func buttonOneClicked(_ sender: UIButton) {
+        if sender.titleLabel?.text == "Great"
+        {
+            emojiLabel.text = "that's fantastic!"
+        }
+        else if sender.titleLabel?.text == "Okay"
+        {
+            emojiLabel.text = "I hope it gets better!"
+        }
+        else {
+            emojiLabel.text = "treat yourself to a coffee"
+        }
+    }
+    
 }
 
